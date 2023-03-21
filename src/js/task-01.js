3e2bc5eae99895b3b365d0688447179137364ccb
irkutsio <117863253+irkutsio@users.noreply.github.com>
2023-03-22 00:39:28 +0200

    task-2,3

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,2 +1,19 @@
-const test = "hello world";
-console.log(test);
+// const title = document.querySelectorAll(".title");
+// // const h2 = item.querySelector("h2");
+// console.dir(title.length);
+// undefined;
+
+// const title = document.querySelector(".title");
+// // const h2 = item.querySelector("h2");
+// title.classList.add("js-title");
+// // title.classList.remove("js-title");
+// title.classList.replace("title", "js-new_title");
+// console.log(title);
+
+// const ul = document.querySelector("ul");
+// console.log(ul);
+
+const newElement = document.createElement("h1");
+console.log(newElement);
+newElement.textContent = "Єто новый элемент";
+document.body.prepend(newElement);
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,43 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientList = document.querySelector("#ingredients");
+
+// const newList = [];
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   const ingredientEl = document.createElement("li");
+//   ingredientEl.textContent = ingredients[i];
+//   ingredientEl.classList.add(".item");
+//   newList.push(ingredientEl);
+// }
+// ingredientList.append(...newList);
+//***********************//
+
+const newList = ingredients.map((ingredient) => {
+  const ingredientEl = document.createElement("li");
+  ingredientEl.textContent = ingredient;
+  ingredientEl.classList.add("item");
+
+  return ingredientEl;
+});
+// console.log(newList);
+ingredientList.append(...newList);
+
+//******************//
+// const createIngredientList = (array) => {
+//   return array.map((ingredient) => {
+//     const ingredientEl = document.createElement("li");
+//     ingredientEl.textContent = ingredient;
+//     ingredientEl.classList.add("item");
+
+//     return ingredientEl;
+//   });
+// };
+// const newList = createIngredientList(ingredients);
+// ingredientList.append(...newList);
